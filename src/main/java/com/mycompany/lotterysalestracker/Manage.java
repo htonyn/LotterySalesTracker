f60bcc66fd4d184930574e77fe556830917dcac5
htonyn-d <htonynguyen@gmail.com>
2017-11-18 01:19:44 -0500

    Updated Manage Tabs

--- src/main/java/com/mycompany/lotterysalestracker/Manage.java
+++ src/main/java/com/mycompany/lotterysalestracker/Manage.java
@@ -1,43 +1,23 @@
 package com.mycompany.lotterysalestracker;
 
-import javafx.geometry.Pos;
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class Manage extends GridPane {
+    GridPane addGameBox;
+    GridPane currentGameBox;
     public Manage() {
-        initialize();
-        Button addGame = new Button("Add Game");
-        Button removeGame = new Button("Remove Game");
-        Button finishBook = new Button("Finish Book");
-        
-        GridPane addGameBox = new GridPane();
-        GridPane addGameControls = new GridPane();
+        addGameBox = new GridPane();
         addGameBox.setPrefHeight(800);
-        
-        addGameBox.add(new Text("Game List"), 0, 0);
-        Region vSpace = new Region();
-        addGameBox.add(vSpace, 0, 2);
-        addGameBox.setVgrow(vSpace, Priority.ALWAYS);
-        addGameBox.setHgrow(vSpace, Priority.ALWAYS);
-        addGameBox.add(addGameControls, 0, 3);
-        
-        addGameControls.add(addGame, 0, 0);
-        Region r = new Region();
-        addGameControls.add(r, 1, 0);
-        addGameControls.setHgrow(r, Priority.ALWAYS);
-        addGameControls.add(removeGame, 2, 0);        
-        setHgrow(addGameBox, Priority.ALWAYS);
-        
-        VBox vbox2 = new VBox(8);
+        currentGameBox = new GridPane();
+        initializeView();
         
         TabPane tp = new TabPane();
         tp.setStyle("-fx-background-color: white;");
@@ -48,13 +28,34 @@ public class Manage extends GridPane {
         gamePage.setContent(addGameBox);
         gamePage.setOnCloseRequest(e -> e.consume());
         Tab currentPage = new Tab("Current Books");
-        currentPage.setContent(vbox2);
+        currentPage.setContent(currentGameBox);
         currentPage.setOnCloseRequest(e -> e.consume());
         
         add(tp, 0, 0, 3, 1);
         setHgrow(tp, Priority.ALWAYS);
         tp.getTabs().addAll(gamePage, currentPage);
-        
+    }
+    
+    public void populateTableView() {
+//        GameTable gt = new GameTable(FXCollections.observableArrayList());
+    }
+    
+    public void initializeView() {
+        GridPane addGameControls = new GridPane();
+        Button addGame = new Button("Add Game");
+        Button removeGame = new Button("Remove Game");
+        addGameBox.add(new Text("Game List"), 0, 0);
+        Region vSpace = new Region();
+        addGameBox.add(vSpace, 0, 2);
+        addGameBox.setVgrow(vSpace, Priority.ALWAYS);
+        addGameBox.setHgrow(vSpace, Priority.ALWAYS);
+        addGameBox.add(addGameControls, 0, 3);
+        addGameControls.add(addGame, 0, 0);
+        Region r = new Region();
+        addGameControls.add(r, 1, 0);
+        addGameControls.setHgrow(r, Priority.ALWAYS);
+        addGameControls.add(removeGame, 2, 0);        
+        setHgrow(addGameBox, Priority.ALWAYS);
         addGame.setOnAction(
             (event) -> {
                 System.out.println("Add Game");
@@ -65,14 +66,40 @@ public class Manage extends GridPane {
                 System.out.println("Remove Game");
             }
         );
-        finishBook.setOnAction(
+        
+        GridPane currentGameControls = new GridPane();
+        Region vSpace1 = new Region();
+        Button addBook = new Button("Add Book");
+        Button removeBook = new Button("Remove Book");
+        Button finishBook = new Button("Finish Book");
+        Region r1 = new Region();
+        Region r2 = new Region();
+        currentGameControls.add(addBook, 0, 0);
+        currentGameControls.add(r1, 1, 0);
+        currentGameControls.add(removeBook, 0, 0);
+        currentGameControls.add(r2, 3, 0);
+        currentGameControls.add(finishBook, 4, 0);
+        currentGameControls.setHgrow(r1, Priority.ALWAYS);
+        currentGameControls.setHgrow(r2, Priority.ALWAYS);
+        currentGameBox.add(new Text("Current Books"), 0, 0);
+        currentGameBox.add(currentGameControls, 0, 3);
+        currentGameBox.add(vSpace1, 0, 3);
+        currentGameBox.setVgrow(vSpace1, Priority.ALWAYS);
+        
+        addBook.setOnAction( 
             (event) -> {
-                System.out.println("Finish Book");
+                System.out.println("Manage: Add Book");
             }
         );
-    }
-    
-    public void initialize() {
-        // TableView
+        removeBook.setOnAction( 
+            (event) -> {
+                System.out.println("Manage: Remove Book");
+            }
+        );
+        finishBook.setOnAction(
+            (event) -> {
+                System.out.println("Manage: Finish Book");
+            }
+        );
     }
 }
